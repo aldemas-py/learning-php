1ba4a246d09c56563a159e13dcb5e6da91e540e3
Aldemas-py <njengasam2406@gmail.com>
2022-09-13 06:14:19 +0300

    to debug

--- laravell_new/app/Models/listing.php
+++ laravell_new/app/Models/listing.php
@@ -2,32 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class listing extends Model
 {
-    public static function all()
-    {
-        return  [
-            [
-                'id' => 1,
-                'tittle' => 'Listing 1',
-                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, mollitia eaque ea autem non eligendi culpa quod suscipit in ipsam ducimus accusamus beatae veritatis vel, laborum voluptates aliquid! Delectus, dignissimos.'
-            ],
-            [
-                'id' => 2,
-                'tittle' => 'Listing 2',
-                'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, mollitia eaque ea autem non eligendi culpa quod suscipit in ipsam ducimus accusamus beatae veritatis vel, laborum voluptates aliquid! Delectus, dignissimos.'
-            ]
-        ];
-    }
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
--- laravell_new/database/seeders/DatabaseSeeder.php
+++ laravell_new/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\listing;
+use Database\Factories\Listing_factoryFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
+
+        Listing::factory(6)->create();
+        // listing::create([
+        //     'tittle' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        // ]);
+        // listing::create([
+        //     'tittle' => 'Full-Stack Engineer',
+        //     'tags' => 'laravel, backend ,api',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York, NY',
+        //     'email' => 'email2@email.com',
+        //     'website' => 'https://www.starkindustries.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        // ]);
+
     }
 }
