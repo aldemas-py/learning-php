6de0bec55fd83afefca62c52d97e9d21c2a38924
Aldemas-py <njengasam2406@gmail.com>
2022-09-13 08:45:16 +0300

    Base system established

--- laravell_new/database/seeders/DatabaseSeeder.php
+++ laravell_new/database/seeders/DatabaseSeeder.php
@@ -37,6 +37,23 @@ class DatabaseSeeder extends Seeder
             'website' => 'https://www.starkindustries.com',
             'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
         ]);
-
+        listing::create([
+            'tittle' => ' Senior  backend Developer',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'email1@email.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        ]);
+        listing::create([
+            'tittle' => 'Frontend Engineer',
+            'tags' => 'laravel, backend ,api',
+            'company' => 'Stark Industries',
+            'location' => 'New York, NY',
+            'email' => 'email2@email.com',
+            'website' => 'https://www.starkindustries.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        ]);
     }
 }
--- laravell_new/resources/views/listings.blade.php
+++ laravell_new/resources/views/listings.blade.php
@@ -1,26 +1,26 @@
-<!-- After blade -->\
+@extends('layout')
 
-<h1> {{$heading;}} </h1>
+@section('content')
 
-{{-- @php    
-    $test=1;
-@endphp --}}
-{{-- {{$test}} --}}
+@include('partials._hero')
+@include('partials._search')
 
-{{-- @if (count($listings) ==0)
-    <p>no listing found</p>
-@endif --}}
+<main>
+
+<div
+class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
+>
+
+
+</div>
 @unless (count($listings) == 0)
 
 @foreach($listings as $listing)
-<h2>
-    <a href="/listings/{{$listing['id']}}">{{$listing['tittle'];}}</a>
-</h2>
-<p>
-    {{$listing['description'];}}
-</p>
+<x-listing-card :listing="$listing"/>
 @endforeach  
 
 @else
     <p>No listings found</p>
 @endunless
+
+@endsection 
