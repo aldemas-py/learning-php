b06fe325783f250642f6e6e28646f704b8432f46
Aldemas-py <njengasam2406@gmail.com>
2022-09-15 18:01:11 +0300

    creating listing

--- laravell_new/app/Http/Controllers/ListingController.php
+++ laravell_new/app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -22,5 +23,28 @@ class ListingController extends Controller
         return view('listings.show', [
             'listing' => $listing
         ]);
+    }
+    // show create form
+    public function create()
+    {
+        return view('listings.create');
+    }
+    // store create data
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $formFileds = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'requred',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        Listing::create($formFileds);
+
+        return redirect('/');
     }
 }
