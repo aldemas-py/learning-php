62508516fe35b4351b29e54c13c10d0bdaf9fdb2
Aldemas-py <njengasam2406@gmail.com>
2022-09-20 16:33:26 +0300

    rettain old data

--- laravell_new/resources/views/listings/create.blade.php
+++ laravell_new/resources/views/listings/create.blade.php
@@ -18,7 +18,7 @@
                 <input
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="company"
+                    name="company" value="{{old('company')}}"
                 />
                 @error('company')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -33,7 +33,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="title"
-                    placeholder="Example: Senior Laravel Developer"
+                    placeholder="Example: Senior Laravel Developer" value="{{old('title')}}"
                 />
                 @error('title')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -50,7 +50,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="location"
-                    placeholder="Example: Remote, Boston MA, etc"
+                    placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}"
                 />
                 @error('location')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -64,7 +64,7 @@
                 <input
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="email"
+                    name="email"value="{{old('email')}}"
                 />
                 @error('email')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -81,7 +81,7 @@
                 <input
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="website"
+                    name="website"value="{{old('website')}}"
                 />
                 @error('website')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -96,7 +96,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="tags"
-                    placeholder="Example: Laravel, Backend, Postgres, etc"
+                    placeholder="Example: Laravel, Backend, Postgres, etc"value="{{old('tags')}}"
                 />
                 @error('tags')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -110,7 +110,7 @@
                 <input
                     type="file"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="logo"
+                    name="logo"value="{{old('logo')}}"
                 />
                 @error('logo')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -128,7 +128,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="description"
                     rows="10"
-                    placeholder="Include tasks, requirements, salary, etc"
+                    placeholder="Include tasks, requirements, salary, etc"value="{{old('description')}}"
                 ></textarea>
                 
                 @error('description')
