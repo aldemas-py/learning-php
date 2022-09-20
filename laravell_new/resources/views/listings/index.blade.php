112b880b7a7471f0497972d66eacca721327bd90
Aldemas-py <njengasam2406@gmail.com>
2022-09-20 16:45:03 +0300

    code refactored

--- laravell_new/resources/views/listings/index.blade.php
+++ laravell_new/resources/views/listings/index.blade.php
@@ -5,9 +5,6 @@
 
 
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
-
-
-    </div>
     @unless (count($listings) == 0)
 
     @foreach($listings as $listing)
@@ -17,5 +14,12 @@
     @else
         <p>No listings found</p>
     @endunless
+
+    </div>
+
+    <div class="mt-6 p-4">
+        {{$listings->links( )}}
+    </div>
+
 
 </x-layout>
